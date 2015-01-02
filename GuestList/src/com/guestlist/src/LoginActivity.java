8444c39228fa4ea072e0324b8f6df117f63d32fc
ranil1985 <dumedha@yahoo.com>
2015-01-02 09:58:57 +0530

    Update LoginActivity.java

--- GuestList/src/com/guestlist/src/LoginActivity.java
+++ GuestList/src/com/guestlist/src/LoginActivity.java
@@ -14,7 +14,7 @@ public class LoginActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		//hgkkjhkjhkjhkjhkjhkhkjhkhkhjhkjhkhk/////////sdfgsdgggdfgdfgdfg
+		//hgkkjhkjhkjhkjhkjhkhkjhkhkhjhkjhkhk/////////sdfgsdgggdfxxxxxxxxxx
 		Button submitButton=(Button) findViewById(R.id.submitt_button);
 
 		submitButton.setOnClickListener(new OnClickListener() {
